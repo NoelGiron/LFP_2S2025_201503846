040f2dc67137ae03293de5413fb911f64cdcb9c6
NoelGiron <mngiron@hotmail.com>
2025-10-25 12:37:42 -0600

    se agregaron al index.js las funciones principales, se modificaron los botones del index y se creo la pagina ayuda.html

--- Proyecto 2/Lenguaje/index.js
+++ Proyecto 2/Lenguaje/index.js
@@ -20,6 +20,8 @@ document.addEventListener("DOMContentLoaded", function () {
     }, 100);
 });
 
+
+// MENU de archivo
 function cargarArchivo(event) {
     const files = event.target.files;
     if (files.length > 0) {
@@ -39,60 +41,34 @@ function cargarArchivo(event) {
     }
 }
 
-function analizarTorneo() {
-    const contenido = editor.getValue();
-    const scanner = new Scanner(contenido);
-    const resultContent= document.getElementById('resultsContent');
-
-   resultContent.innerHTML = `<div class="loading">Analizando</div>`;
-
-    let tokenCount = 1;
-    let token = scanner.next_token();
-
-    const tabla = document.createElement('table');
-    tabla.className = 'tabla-token';
-    tabla.innerHTML = `
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>Token</th>
-          <th>Tipo</th>
-          <th>Linea</th>
-          <th>Columna</th>
-          <th>Lexema</th>
-        </tr>
-      </thead>
-      <tbody id="tokensBody"></tbody>
-    `;
-
-   resultContent.innerHTML = '';
-   resultContent.appendChild(tabla);
-    const tokensBody = document.getElementById('tokensBody');
-
-    while(token && token.type !== 'EOF') {
-        const row = document.createElement('tr');
-
-        row.innerHTML = `
-          <td>${tokenCount}</td>
-          <td>${token.type}</td>
-          <td>${token.type.startsWith('KW_') ? 'palabra clave' :
-              token.type.startsWith('TK_') ? 'simbolo' :
-              token.type === 'TK_string' ? 'cadena' :
-              token.type === 'TK_id' ? 'identificador' : token.type}</td>
-          <td>${token.line}</td>
-          <td>${token.column}</td>
-          <td>${token.lexeme || token.type}</td>
-
-        `;
-        tokensBody.appendChild(row);
-        tokenCount++;
-        token = scanner.next_token();
-    }
+function limpiarEditor() {
     
 }
-function generarReporte() {
+
+function guardarJava() {
 
 }
-function mostrarBracket() {
+
+function guardarPython() {
 
 }
+
+function salir(){
+
+}
+
+//MENU del traductor
+
+function generarTraduccion(){
+
+}
+
+function verTokens(){
+
+}
+
+function simularEjecucion(){
+
+}
+
+
