188bb7b7a6bf199e2420c188b62d6511b2ae1ead
NoelGiron <mngiron@hotmail.com>
2025-10-25 21:36:37 -0600

    se creo la carpeta Expressions y la carpeta Utils con el archivo Type

---
+++ Proyecto 2/Interpreter/Utils/Type.js
@@ -0,0 +1,7 @@
+const Type = {
+    INT:     {value: 'int',    pythonType: 'int',    defaultValue: '0',      ord: 0},
+    DOUBLE:  {value: 'double', pythonType: 'float',  defaultValue: '0.0',    ord: 1},
+    STRING:  {value: 'String', pythonType: 'str',    defaultValue: '""',     ord: 2},
+    BOOLEAN: {value: 'boolean',pythonType: 'bool',   defaultValue: 'False',  ord: 3},
+    CHAR:    {value: 'char',   pythonType: 'str',    defaultValue: "''",     ord: 4},
+}
